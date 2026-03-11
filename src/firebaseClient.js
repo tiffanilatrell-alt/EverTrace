29e168a3ebcffd349cf16c08d12bf013c099f7c4
Tiffani Norris <tiffani.latrell!@gmail.com>
2026-03-10 22:52:27 -0400

    initial EverTrace setup

---
+++ src/firebaseClient.js
@@ -0,0 +1,16 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  projectId: "evertrace-660fc",
+  storageBucket: "evertrace-660fc.firebasestorage.app",
+  authDomain: "evertrace-660fc.firebaseapp.com",
+  apiKey: "AIzaSyDsoBnfgKt1rWaWFCEIThqOjcAMXySSs4M",
+  appId: "1:757938126442:web:1a3f52259e5bf6d8485413"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
